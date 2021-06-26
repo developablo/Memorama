c39b303bc025d3a5d3d10996b5f7d3d5386f5595
Pablo <pablo.esquivel@hotmail.com>
2021-06-26 07:51:20 -0300

    Card model created, testing images on cards

--- src/app/game/components/board/board.component.ts
+++ src/app/game/components/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../../models/card.model';
 
 @Component({
   selector: 'app-board',
@@ -7,7 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  public cards: string[] = ['A','B','C','D','E','F','G','H','I','J','A','B','C','D','E','F','G','H','I','J']
+  private availableCards: Card[] = [
+    {
+      id: 1,
+      revealed: false,
+      imageUrl: 'assets/images/arrow.svg',
+      cardName: 'Flecha'
+    },
+    {
+      id: 2,
+      revealed: false,
+      imageUrl: 'assets/images/brush.svg',
+      cardName: 'Pincel'
+    },
+    {
+      id: 3,
+      revealed: false,
+      imageUrl: 'assets/images/camera.svg',
+      cardName: 'Cámara'
+    },
+    {
+      id: 4,
+      revealed: false,
+      imageUrl: 'assets/images/fish.svg',
+      cardName: 'Pez'
+    },
+    {
+      id: 5,
+      revealed: false,
+      imageUrl: 'assets/images/laptop.svg',
+      cardName: 'Laptop'
+    },
+    {
+      id: 6,
+      revealed: false,
+      imageUrl: 'assets/images/lightbulb.svg',
+      cardName: 'Lámpara'
+    },
+    {
+      id: 7,
+      revealed: false,
+      imageUrl: 'assets/images/mortarboard-hat.svg',
+      cardName: 'Bonete'
+    },
+    {
+      id: 8,
+      revealed: false,
+      imageUrl: 'assets/images/owl.svg',
+      cardName: 'Buho'
+    },
+    {
+      id: 9,
+      revealed: false,
+      imageUrl: 'assets/images/popsicles.svg',
+      cardName: 'Helados'
+    },
+    {
+      id: 10,
+      revealed: false,
+      imageUrl: 'assets/images/flower.svg',
+      cardName: 'Flor'
+    }
+  ]
+  public cards: Card[] = [this.availableCards[0],this.availableCards[1],this.availableCards[2],this.availableCards[3],this.availableCards[4],this.availableCards[5],this.availableCards[6],this.availableCards[7],this.availableCards[8],this.availableCards[9]]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/game/models/card.model.ts
@@ -0,0 +1,6 @@
+export class Card {
+  id: number;
+  revealed: boolean;
+  imageUrl: string;
+  cardName:string;
+}
